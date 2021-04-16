b51a06c783c8d5615c5b4d73b2764451fae7032c
RonaldColyar <phantexcreations@gmail.com>
2021-04-16 03:08:42 -0400

    added decryption

--- src/main.rs
+++ src/main.rs
@@ -19,11 +19,26 @@ fn main(){
     let mut running = true;
     let mut ui = interface::Interface::new();
     print!("\x1B[2J\x1B[1;1H");  //clears console
+    let path = 
+    env::current_dir()
+    .unwrap()
+    .to_str()
+    .unwrap()
+    .to_owned() + "/src/test.txt";
+    let path2 = 
+    env::current_dir()
+    .unwrap()
+    .to_str()
+    .unwrap()
+    .to_owned() + "/src/test1.txt";
 
     
     ui.router.logger.first_start_message();
     ui.start();
     
- 
+    let result = encryptor::encrypt("iloveyou" ,path2 ,path );
+    if result.is_ok(){
+        println("works well");
+    }
     
 }
